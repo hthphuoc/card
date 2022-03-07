562734bafe5fd899f0e640f2487373615add118b
hthphuoc <101106786+hthphuoc@users.noreply.github.com>
2022-03-07 23:10:44 +0700

    Add files via upload
    creat card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Huyền Trang';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'Tặng Huyền Trang';
+const contentCard = 'Chúc Trang 08-03 vui vẻ, gặp nhiều may mắn, ngày càng xinh đẹp. À ngày càng xinh lão nhá :v';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'trang.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
